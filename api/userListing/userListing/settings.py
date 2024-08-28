438e8485f12f703db04ede697789959dfa19ae4b
shantanu-02 <shantanupawar101@gmail.com>
2024-08-29 03:01:04 +0530

    Fix: Changed URL endpoints

--- api/userListing/userListing/settings.py
+++ api/userListing/userListing/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users',
+    'corsheaders',
+    'users', #project's internal App
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,5 +128,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2  # Adjust the page size as needed
+    'PAGE_SIZE': 10  # Adjust the page size as needed
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+    'HEAD',
+]
--- api/userListing/users/urls.py
+++ api/userListing/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import UserListView, UserDetailView, UserSearchView
+from .views import UserListView, UserDetailView, UserSearchView, FetchAndStoreUserData
 
 urlpatterns = [
-    path('', UserListView.as_view(), name='user-list'),  # For paginated user list
+    path('', UserListView.as_view(), name='user-list'),
+    path('fetch-random-users/', FetchAndStoreUserData.as_view(), name='fetch_store_users'),
     path('<int:id>/', UserDetailView.as_view(), name='user-detail'),
     path('search/', UserSearchView.as_view(), name='user-search')
 ]
