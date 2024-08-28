64a6edc7a833c722d864f482d783397fdfa2290f
shantanu-02 <shantanupawar101@gmail.com>
2024-08-29 03:01:34 +0530

    Feat: Added DB indexing

--- api/userListing/users/models.py
+++ api/userListing/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Users(models.Model):
-    gender = models.CharField(max_length=10, db_index=True)
+    gender = models.CharField(max_length=10)
     title = models.CharField(max_length=10)
     first_name = models.CharField(max_length=50, db_index=True)
     last_name = models.CharField(max_length=50, db_index=True)
@@ -15,17 +15,17 @@ class Users(models.Model):
     longitude = models.FloatField()
     timezone_offset = models.CharField(max_length=10)
     timezone_description = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, db_index=True)
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=255)
     dob = models.DateTimeField()
     registered = models.DateTimeField()
     phone = models.CharField(max_length=20)
-    cell = models.CharField(max_length=20)
+    cell = models.CharField(max_length=20, db_index=True)
     nat = models.CharField(max_length=5)
     picture_large = models.URLField()
     picture_medium = models.URLField()
     picture_thumbnail = models.URLField()
 
-    # def __str__(self):
-    #     return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
